cf24724f449e37bb72288ee171a6f7dee5a88070
Юлия Ушенина <ushenina.julia@gmail.com>
2022-04-28 11:57:41 +0300

    5 bugs

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,33 +1,24 @@
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
+from django.core.exceptions import ValidationError
+from django.shortcuts import get_object_or_404
 
-from reviews.models import Category, Comment, Genre, Review, Title, User # noqa
+from reviews.models import Category, Comment, Genre, Review, Title, User
 
 
 class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
-            'username',
-            'email',
-            'first_name',
-            'last_name',
-            'bio',
-            'role'
-        )
+            'username', 'email', 'first_name',
+            'last_name', 'bio', 'role')
 
 
 class NotAdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
-            'username',
-            'email',
-            'first_name',
-            'last_name',
-            'bio',
-            'role'
-        )
+            'username', 'email', 'first_name',
+            'last_name', 'bio', 'role')
         read_only_fields = ('role',)
 
 
@@ -41,7 +32,7 @@ class GetTokenSerializer(serializers.ModelSerializer):
         model = User
         fields = (
             'username',
-            'confirmation_code',
+            'confirmation_code'
         )
 
 
@@ -107,34 +98,34 @@ class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         slug_field='username',
         read_only=True,
-        default=serializers.CurrentUserDefault
+        default=serializers.CurrentUserDefault()
     )
-
+    
     class Meta:
         model = Comment
         exclude = ('review',)
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-    title_id = serializers.SlugRelatedField(
-        slug_field='id',
-        queryset=Title.objects.all()
+    title = serializers.SlugRelatedField(
+        slug_field='name',
+        read_only=True
     )
     author = serializers.SlugRelatedField(
         slug_field='username',
-        default=serializers.CurrentUserDefault,
         read_only=True
     )
 
     def validate(self, data):
-        if self.context['request'].method == 'POST':
-            user = self.context['request'].user
-            title_id = self.context['view'].kwargs.get('title_id')
-            print('CONTEXT------------------------------------------------------------')
-            if Review.objects.filter(author=user, title_id=title_id).exists():
-                raise ValidationError(
-                    'Вы уже написали отзыв на это произведение'
-                )
+        request = self.context['request']
+        author = self.context['request'].user
+        title_id = self.context.get('view').kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        if (
+            request.method == 'POST'
+            and Review.objects.filter(title=title, author=author).exists()
+        ):
+            raise ValidationError('Вы уже написали отзыв на это произведение')
         return data
 
     class Meta:
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -9,19 +9,21 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from reviews.models import Category, Comment, Genre, Review, Title, User # noqa
-from .permissions import AdminOnly, IsAdminUserOrReadOnly
+from reviews.models import Category, Comment, Genre, Review, Title, User
+from .permissions import AdminOnly, AdminModeratorAuthorPermission, IsAdminUserOrReadOnly
 from .mixins import ListCreateDestroyViewSet
 from .filters import TitlesFilter
 from .serializers import (
     CategorySerializer,
     CommentSerializer,
     GenreSerializer,
+    TitleWriteSerializer,
+    TitleReadSerializer,
     GetTokenSerializer,
     NotAdminSerializer,
     ReviewSerializer,
     SignUpSerializer,
-    UsersSerializer, TitleReadSerializer, TitleWriteSerializer
+    UsersSerializer
 )
 
 
@@ -153,15 +155,15 @@ class TitleViewSet(viewsets.ModelViewSet):
             return TitleReadSerializer
         return TitleWriteSerializer
 
-
 class CommentViewset(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (IsAdminUserOrReadOnly,)
+    permission_classes = (AdminModeratorAuthorPermission,)
 
     def get_queryset(self):
         review_id = int(self.kwargs.get('review_id'))
         review = get_object_or_404(Review, pk=review_id)
         return review.comments
+
 
     def preform_create(self, serializer):
         review_id = int(self.kwargs.get('review_id'))
@@ -172,11 +174,13 @@ class CommentViewset(viewsets.ModelViewSet):
 
 class ReviewViewset(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
+    permission_classes = (AdminModeratorAuthorPermission,)
 
     def get_queryset(self):
         title_id = int(self.kwargs.get('title_id'))
         title = get_object_or_404(Title, pk=title_id)
-        return title.reviews
+        return title.reviews.all()
+
 
     def perform_create(self, serializer):
         title_id = int(self.kwargs.get('title_id'))
